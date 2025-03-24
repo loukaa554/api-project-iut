a5a4cc9e4dca2bcb7082dc1bdd9e2273e451c98d
Loukaa <louka.mourrier@gmail.com>
2025-03-24 12:36:47 +0100

    fix

--- js/functions/url.js
+++ js/functions/url.js
@@ -1,6 +1,6 @@
 export const getBaseUrl = () => {
   let base = "";
-  if (window.location.origin.includes("loukaa554.github.io")) {
+  if (window.location.origin.includes("github.io")) {
     // alors récupérer le /nom-du-repo/
     base = window.location.pathname.split("/")[1];
   }
